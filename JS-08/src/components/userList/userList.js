3e35c4c4de032607247590a5a115462a0512284f
skyrocker <skyrocker2013@gmail.com>
2017-07-27 16:55:15 +0300

    make userList stateless

--- JS-08/src/components/userList/userList.js
+++ JS-08/src/components/userList/userList.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
-// import ReactDOM from 'react-dom'
 import User from '../user/user'
 import './userList.css'
 
 class UserList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: props.users
-        }
-    }
-
     handleDel = (user) => {
         this.props.onUserDelete(user)
     };
